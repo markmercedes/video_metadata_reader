154ca771303eff1fbfbd074b26f28a8fe7bae891
Briam Santiago <bm.santiago14@gmail.com>
2015-10-06 16:39:25 -0400

    Raise and handle exception when the file path is invalid.

--- app/controllers/video_info_controller.rb
+++ app/controllers/video_info_controller.rb
@@ -1,10 +1,23 @@
+require 'invalid_path'
+
 class VideoInfoController < ApplicationController
 
+  rescue_from InvalidPath, with: :invalid_path
+
   def metadata
-    video_meta = Rails.cache.fetch("/video/#{video_path}", :expires_in => nil) { FFMPEG::Movie.new(video_path) }
+    video_meta = Rails.cache.fetch("/video/#{video_path}", :expires_in => nil) do
+      result = FFMPEG::Movie.new(video_path).as_json
+      raise InvalidPath.new if result["duration"] == 0
+      result
+    end
+
     respond_to do |format|
       format.json { render json: video_meta }
     end
+  end
+
+  def invalid_path
+    render json: { message: 'Invalid Path' }, status: 400
   end
 
   private
